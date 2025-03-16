8fb51051e0261c10edac53d67b77fe8da052900b
Orz0107 <zhuo0107@foxmail.com>
2025-03-16 23:33:50 +0800

    {yy:MM:dd}

--- themes/cactus/source/js/main.js
+++ themes/cactus/source/js/main.js
@@ -68,7 +68,7 @@ $(document).ready(function() {
         var topDistance = document.documentElement.scrollTop;// menu.offset().top;
 
         // hide only the navigation links on desktop
-        const distanceLimit = 50;
+        const distanceLimit = 100;
         if (!nav.is(":visible") && topDistance < distanceLimit) {
           nav.show();
         } else if (nav.is(":visible") && topDistance >= distanceLimit) {
