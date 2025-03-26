808fd96629a9d9ae4f80c648a7345e288e3514e4
Orz0107 <zhuo0107@foxmail.com>
2025-03-26 12:48:46 +0800

    修改网站 icon

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,6 @@
 import IconMail from "@/assets/icons/IconMail.svg";
 import IconGitHub from "@/assets/icons/IconGitHub.svg";
 import IconBrandX from "@/assets/icons/IconBrandX.svg";
-import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
 import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
@@ -39,37 +38,37 @@ export const SHARE_LINKS = [
   {
     name: "WhatsApp",
     href: "https://wa.me/?text=",
-    linkTitle: `Share this post via WhatsApp`,
+    linkTitle: `分享到 WhatsApp`,
     icon: IconWhatsapp,
   },
   {
     name: "Facebook",
     href: "https://www.facebook.com/sharer.php?u=",
-    linkTitle: `Share this post on Facebook`,
+    linkTitle: `分享到 Facebook`,
     icon: IconFacebook,
   },
   {
     name: "X",
     href: "https://x.com/intent/post?url=",
-    linkTitle: `Share this post on X`,
+    linkTitle: `分享到 X`,
     icon: IconBrandX,
   },
   {
     name: "Telegram",
     href: "https://t.me/share/url?url=",
-    linkTitle: `Share this post via Telegram`,
+    linkTitle: `通过 Telegram 分享`,
     icon: IconTelegram,
   },
   {
     name: "Pinterest",
     href: "https://pinterest.com/pin/create/button/?url=",
-    linkTitle: `Share this post on Pinterest`,
+    linkTitle: `分享到 Pinterest`,
     icon: IconPinterest,
   },
   {
     name: "Mail",
     href: "mailto:?subject=See%20this%20post&body=",
-    linkTitle: `Share this post via email`,
+    linkTitle: `通过 email 分享`,
     icon: IconMail,
   },
 ] as const;
--- src/pages/about.md
+++ src/pages/about.md
@@ -7,6 +7,6 @@ title: "关于我"
 
 目前正在准备复试和毕设。
 
-如果想联系请使用邮件，谢谢~
+如果想联系请使用邮件或者底下评论区，谢谢~
 
 ![喵喵](https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcHRxdGhsNWtqaHRpYWxicDdmZXAxNnc5dDVnZGU1MTdueXRxNWNuYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/npRnc6r5vhwV7kdmhd/giphy.gif)
